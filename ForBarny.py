961dc673df111d94e6108f6bc71c1878d8981f65
scerbinrie <twintips1999@gmail.com>
2016-08-01 20:32:51 -0400

    Add files via upload

---
+++ ForBarny.py
@@ -0,0 +1,16 @@
+print "Hello Barny!"
+print "I heard you have some questions?"
+print "Please input the temperature."
+# Python Program to convert temperature in celsius to fahrenheit 
+# Input is provided by the user in degree celsius
+
+# take input from the user
+celsius = float(input('Enter degree: '))
+Fahrenheit = float(input('Enter degree: '))
+
+Celsius = (Fahrenheit - 32) * 5.0/9.0
+
+print "If Fahrenheit:", Fahrenheit, "Fahrenheit = ", Celsius, " Celsius"
+# calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+print('If Celsius %0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
