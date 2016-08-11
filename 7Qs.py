cdd61397f50dc010efb75a0e1b4eb917357c9432
barnysanchez <barnyrunner@gmail.com>
2016-08-10 20:23:06 -0400

    Update 7Qs.py
    Revise my commentary.

--- 7Qs.py
+++ 7Qs.py
@@ -3,4 +3,17 @@ print "Please think of a number between 0-100."
 print "Got it?"
 print "Good."
 
-print "Ok, first question is the number greater than or equal or less than 50"
+print "Ok, first question is the number greater than or equal or less than 50"    ''' <--- hmm, there are three implied quetions in this. 
+
+While you could make a program that worked to this kind of questioning, the response provided by the user could be many, so you would have to
+code an interpreter of the responses or limit the scope of the user's responses. For example I could say "bigger" or "greater" or "smaller" 
+or "lesser" or "less".
+
+For the purposes of simplicity, narrow the scope and modify the language so that the user would have to response "Yes" or "No". 
+
+For example: "Is the number greater than or equal to 50 (Yes/No)?"   
+
+Doing it this way makes the binary tree decision also a lot simpler to conceptualize. 
+
+
+'''
