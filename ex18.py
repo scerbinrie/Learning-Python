6c1a87a064ee52cb3c5b1ab030bbaf69d4477d4e
scerbinrie <twintips1999@gmail.com>
2016-08-12 21:13:57 -0400

    new
    new programs
    fixed old program ex18.py
    issues with new program ex20.py , the file is written perfectly but i
    am having an issue with the text file so that when i run the program
    the terminal acts as if I had just hit enter any ideas?

--- ex18.py
+++ ex18.py
@@ -9,7 +9,7 @@ def print_two_again(arg1, arg2):
 
 	# this is takes one argument
 def print_one(arg1):
-	print "arg1: %r" % agr1
+	print "arg1: %r" % arg1
 
 	# this one takes no arguments
 def print_none():
